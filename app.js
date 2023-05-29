172c1bd6deeb977965e93756a6cf51092624b37f
Anitap <123569163+Anitapppp@users.noreply.github.com>
2023-05-29 11:56:09 -0500

    29/05/23

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ let puntaje = document.getElementById("puntaje");
 
 let resultado = 0;
 let tiempoactual = tiempofaltante.textContent;
+let posiciontopo = null;
+
+function cuadradoazar() {
+cuadtrado.forEach((nombredeclase) => {
+  nombredeclase.classlist.remove("topo");
+});
+
+let posicionalazar = cuadrado[Math.floor (Math.random()*9];
+posicionalazar.classlist.add("topo");
+
+posiciontopo = posicionalazar.id;
