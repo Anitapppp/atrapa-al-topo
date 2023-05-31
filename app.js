3bae74d682b3602742fe6f90dd8b579b06f14235
Anitap <123569163+Anitapppp@users.noreply.github.com>
2023-05-31 10:31:10 -0500

    31/05/23

--- app.js
+++ app.js
@@ -7,11 +7,37 @@ let tiempoactual = tiempofaltante.textContent;
 let posiciontopo = null;
 
 function cuadradoazar() {
-cuadtrado.forEach((nombredeclase) => {
-  nombredeclase.classlist.remove("topo");
+  cuadrado.forEach((nombredeclase) => {
+    nombredeclase.classlist.remove("topo");
+  });
+
+  let posicionalazar = cuadrado[Math.floor(Math.random() * 9)];
+  posicionalazar.classlist.add("topo");
+
+  posiciontopo = posicionalazar.id;
+}
+cuadrado.forEach((identificador) => {
+  identificador.addEventListener("click", () => {
+    if (identificador.id === posiciontopo) {
+      resultado = resultado + 1;
+      puntaje.textContent = resultado;
+      posicionalazar = null;
+    }
+  });
 });
 
-let posicionalazar = cuadrado[Math.floor (Math.random()*9];
-posicionalazar.classlist.add("topo");
+function movereltopo() {
+  tiempotopo = setInterval(cuadradoazar, 700);
+}
+movereltopo();
 
-posiciontopo = posicionalazar.id;
+function cuentaregresiva() {
+  tiempoactual--;
+  tiempofaltante.textContent = tiempoactual;
+  if (tiempoactual === 0) {
+    clearInterval(idtiempo);
+    clearInterval(tiempotopo);
+    alert("Se acabo el tiempo, tu puntaje es" + resultado + "topos atrapados");
+  }
+}
+let idtiempo = setInterval(cuentaregresiva, 1000);
